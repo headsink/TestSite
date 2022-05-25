d32e3881b1f37fdb6e3cd5bfd106c1ecbe1e9a6b
headsink <headsink@gmail.com>
2022-05-25 22:00:03 +0800

    Format with prettier

--- first.js
+++ first.js
@@ -1,6 +1,5 @@
-const simpleIcons = require('simple-icons');
-const icon = simpleIcons.Get('hive');
-
+const simpleIcons = require("simple-icons");
+const icon = simpleIcons.Get("hive");
 
 console.log(Object.getOwnPropertyNames(icon).toString());
 console.log(icon.title);
