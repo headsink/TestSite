65badd7bb0b1f221d85c43a1ecf31f051528fd6b
headsink <headsink@gmail.com>
2022-06-16 04:44:02 +0800

    formatInTimeZone added

--- datetimetest.js
+++ datetimetest.js
@@ -1,7 +1,9 @@
 const parse = require('date-fns/parse');
 const formatISO = require('date-fns/formatISO');
+const { formatInTimeZone } = require('date-fns-tz');
 
 const b = parse('Mon Jul 16 01:28:17 +0000 2012','EEE LLL dd HH:mm:ss xxxx yyyy' ,new Date());
-const result = formatISO(new Date(b))
+const result = formatISO(new Date(b));
+console.log(formatInTimeZone(result, 'Factory', 'yyyy-MM-dd HH:mm:ss'));
 console.log(result); // 2012-07-16T09:28:17+08:00
 
