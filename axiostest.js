43554ef5cfb582e293e178f34ddcbb7265f78f73
headsink <headsink@gmail.com>
2022-07-08 21:28:33 +0800

    axios test

---
+++ axiostest.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+/*
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+*/
+const getJson = async () => {
+    try{
+
+    }catch(error){
+        
+    }
+}
