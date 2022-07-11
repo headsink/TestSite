5b72727ad50ddbcd6d12c99481c1f152272f0348
headsink <headsink@gmail.com>
2022-07-11 09:56:14 +0800

    axios understood

--- axiostest.js
+++ axiostest.js
@@ -10,14 +10,23 @@ async function getUser() {
   }
 }
 */
-const getJson = async () => {
+
+let datas;
+async function getJson() {
+  
     try{
-      const response = await axios.get("https://headsink.github.io/jsonfile/response_1657182877432.json");
-      console.log("succes");
+      const response = await axios.get("https://headsink.github.io/jsonfile/response_1657182877432.json")
+      .then((res)=>{return res.data;
+      });
+      console.log(response);
+      return response;
 
     }catch(error){
         console.error(error);
     }
 }
-const theJSON = getJson();
-console.log(theJSON);
+const theJSON = getJson().then((value)=>{
+  datas=value;
+  console.log(value);});
+setTimeout(()=>{console.log(JSON.stringify(datas));},500);
+//console.log(JSON.stringify(theJSON.total));
