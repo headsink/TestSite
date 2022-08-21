bbc6893d4790ab2a020a93c5f33de4b7693597dc
headsink <headsink@gmail.com>
2022-08-21 09:36:43 +0800

    getBlogEntries

--- HiveJSTest.js
+++ HiveJSTest.js
@@ -1,15 +1,20 @@
 const hivejs = require('@hiveio/hive-js');
-
 const getPosts = async () => {
 try{
-	await hivejs.api.getBlog("headsink", 10, 3, function(err, data) {
-		console.log(err, data, "success");
+	await hivejs.api.getBlogEntries("fdgdfgdfgdfg", 10, 3, function(err, data) {
+		if(!data[0].blog){
+			console.log(err,"fail");
+		}else{
+			console.log(err,"success");
+			console.table(data);
+		}
+		console.timeEnd("getPost time:");
 	});
 }
 catch{
 	console.error("dead " + err);
 }
 }
-
+console.time("getPost time:");
 getPosts();
 
